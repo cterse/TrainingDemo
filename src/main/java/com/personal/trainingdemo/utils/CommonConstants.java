8b45c277d959a5262a73dd67f302058fae89a094
neil4droid <cterse@gmail.com>
2019-08-04 21:09:10 +0530

    Added Common Constants

---
+++ src/main/java/com/personal/trainingdemo/utils/CommonConstants.java
@@ -0,0 +1,13 @@
+package com.personal.trainingdemo.utils;
+
+import java.util.Date;
+
+public class CommonConstants {
+	
+	private CommonConstants() {
+		throw new IllegalStateException("Attempt to instantiate a utility class");
+	}
+	
+	public static final Date CURRENT_DATE = new Date();
+
+}
